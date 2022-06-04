49f67f3039972aef468f1221db8896ad77b065d9
Vilkov Aleksandr <vilkovaleksandr@gmail.com>
2022-06-05 00:53:12 +0300

    Реализация AnalyticsController

--- src/main/java/com/vilkov/PriceMonitoring/controllers/AnalyticsController.java
+++ src/main/java/com/vilkov/PriceMonitoring/controllers/AnalyticsController.java
@@ -6,7 +6,6 @@ import com.vilkov.PriceMonitoring.model.entity.BaseEntity;
 import com.vilkov.PriceMonitoring.model.entity.Client;
 import com.vilkov.PriceMonitoring.model.entity.FixedPrice;
 import com.vilkov.PriceMonitoring.model.entity.Product;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,14 +21,14 @@ import java.util.TreeSet;
 public class AnalyticsController {
 
     @GetMapping("/getDynamicPrice/{clientID}")
-    public    Map<String, TreeSet<FixedPrice>> getDynamicPrice(@PathVariable String clientID) {
+    public Map<String, TreeSet<FixedPrice>> getDynamicPrice(@PathVariable String clientID) {
         List<BaseEntity> baseEntityList = ProductHelper.readProductInDataBase(new Client(clientID));
         List<Product> products = new ArrayList<>();
-      for (BaseEntity baseEntity: baseEntityList) {
-          if (baseEntity instanceof Product) {
-              products.add((Product) baseEntity);
-          }
-      }
+        for (BaseEntity baseEntity : baseEntityList) {
+            if (baseEntity instanceof Product) {
+                products.add((Product) baseEntity);
+            }
+        }
         return ProductHelper.getSortedPrices(products);
     }
 
