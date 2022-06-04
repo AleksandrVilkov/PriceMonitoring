0192d935f929bd465cd305b444bbab22d9512c20
Vilkov Aleksandr <vilkovaleksandr@gmail.com>
2022-06-05 00:16:13 +0300

    Оптимизация импорта

--- src/main/java/com/vilkov/PriceMonitoring/controllers/ControllerHelper.java
+++ src/main/java/com/vilkov/PriceMonitoring/controllers/ControllerHelper.java
@@ -1,7 +1,5 @@
 package com.vilkov.PriceMonitoring.controllers;
 
-import com.vilkov.PriceMonitoring.dataBaseAdapter.DataBaseAdapter;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,6 +7,7 @@ import java.util.logging.Logger;
 
 public class ControllerHelper {
     private static final Logger logger = Logger.getLogger(ControllerHelper.class.toString());
+
     public static Date getDateFromString(String stringDate) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         try {
--- src/main/java/com/vilkov/PriceMonitoring/controllers/ProductController.java
+++ src/main/java/com/vilkov/PriceMonitoring/controllers/ProductController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/product")
@@ -43,7 +42,7 @@ public class ProductController {
             if (baseEntity instanceof Product) {
                 Date productDate = ((Product) baseEntity).getDate();
                 if (!productDate.before(startDate) && !productDate.after(endDate))
-                products.add((Product) baseEntity);
+                    products.add((Product) baseEntity);
             }
         }
 
