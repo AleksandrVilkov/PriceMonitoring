9bd5851cdf7574e3aa74e6b86a60cb9193d59d29
Vilkov Aleksandr <vilkovaleksandr@gmail.com>
2022-06-04 21:53:27 +0300

    подготовка контролеров

--- src/main/java/com/vilkov/PriceMonitoring/controllers/AnalyticsController.java
+++ src/main/java/com/vilkov/PriceMonitoring/controllers/AnalyticsController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/analytics")
 public class AnalyticsController {
-    //TODO получить PDF по выбранным товарам
+
     //TODO посмотреть динамику конкретного продукта за конкретный период
+    //TODO получить PDF по выбранным товарам
+
     //TODO Получить динамику цен по конкрктному продукту
     //TODO посмотреть динамику всех продуктов за конкретный период
 }
