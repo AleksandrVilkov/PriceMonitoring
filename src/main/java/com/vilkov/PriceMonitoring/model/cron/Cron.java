250d3f876922d110bc0bf1cfca9b780a6a0973cc
Vilkov Aleksandr <vilkovaleksandr@gmail.com>
2022-06-13 00:20:58 +0300

    правка импорта

--- src/main/java/com/vilkov/PriceMonitoring/model/cron/Cron.java
+++ src/main/java/com/vilkov/PriceMonitoring/model/cron/Cron.java
@@ -3,14 +3,12 @@ package com.vilkov.PriceMonitoring.model.cron;
 import com.vilkov.PriceMonitoring.model.ProductHelper;
 import com.vilkov.PriceMonitoring.model.dataStorage.DataStorage;
 import com.vilkov.PriceMonitoring.model.entity.Client;
-import org.bouncycastle.cert.dane.DANECertificateFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.logging.Logger;
