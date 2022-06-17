696592ef5deeb50e997e817294cf5a13f0834481
vilkov <alexander.vilkov@gridnine.com>
2022-06-17 17:35:17 +0300

    Рефактор парсера

--- src/main/java/com/vilkov/PriceMonitoring/model/ProductHelper.java
+++ src/main/java/com/vilkov/PriceMonitoring/model/ProductHelper.java
@@ -2,16 +2,14 @@ package com.vilkov.PriceMonitoring.model;
 
 import com.vilkov.PriceMonitoring.model.dataStorage.DataStorage;
 import com.vilkov.PriceMonitoring.model.entity.*;
-import com.vilkov.PriceMonitoring.model.parsers.ParserHelper;
+import com.vilkov.PriceMonitoring.model.parsers.HTMLPageParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
-import java.util.logging.Logger;
 
 @Component
 public class ProductHelper {
-    private static final Logger logger = Logger.getLogger("ProductHelper");
 @Autowired
 DataStorage dataStorage;
 
@@ -51,7 +49,7 @@ DataStorage dataStorage;
             if (baseEntity instanceof MonitoringList) {
                 List<String> urls = ((MonitoringList) baseEntity).getUrls();
                 for (String url : urls) {
-                    result.add(ParserHelper.getProduct(url));
+                    result.add(HTMLPageParser.searchProduct(url));
                 }
             }
         }
---
+++ src/test/java/com/vilkov/PriceMonitoring/model/parsers/HTMLPageParserTest.java
@@ -0,0 +1,37 @@
+package com.vilkov.PriceMonitoring.model.parsers;
+
+import com.vilkov.PriceMonitoring.model.Status;
+import com.vilkov.PriceMonitoring.model.entity.Currency;
+import com.vilkov.PriceMonitoring.model.entity.Product;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class HTMLPageParserTest {
+
+    @Test
+    public void getProductTest() {
+        String URL = "https://www.perekrestok.ru/cat/366/p/smes-salatnaa-belaa-daca-tango-mix-65g-3451379";
+        Product perecrectokProduct = HTMLPageParser.searchProduct(URL);
+        Assert.assertNotNull(perecrectokProduct);
+        Assert.assertNotNull(perecrectokProduct.getPrice());
+        Assert.assertEquals(perecrectokProduct.getPrice().getCurrency(), Currency.RUB);
+        Assert.assertEquals(perecrectokProduct.getMessage().getStatus(), Status.SUCCESS);
+
+        URL = "https://www.globus.ru/products/93301767_KG/";
+        Product globusProduct = HTMLPageParser.searchProduct(URL);
+        Assert.assertNotNull(globusProduct);
+        Assert.assertNotNull(globusProduct.getPrice());
+        Assert.assertEquals(globusProduct.getPrice().getCurrency(), Currency.RUB);
+        Assert.assertEquals(globusProduct.getMessage().getStatus(), Status.SUCCESS);
+
+//        URL = "https://www.citilink.ru/product/smartfon-huawei-y5p-32gb-chernyi-3g-4g-5-45-and10hms-wifi-gps-1380431/";
+//        Product citilinkProduct = HTMLPageParser.searchProduct(URL);
+//        Assert.assertNotNull(citilinkProduct);
+//        Assert.assertNotNull(citilinkProduct.getPrice());
+//        Assert.assertEquals(citilinkProduct.getPrice().getCurrency(), Currency.RUB);
+//        Assert.assertEquals(citilinkProduct.getMessage().getStatus(), Status.SUCCESS);
+
+
+    }
+
+}
