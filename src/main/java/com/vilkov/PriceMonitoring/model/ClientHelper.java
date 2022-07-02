0b3b89ae1cac178bf7b425fe9197a4963c3267e6
Vilkov Aleksandr <vilkovaleksandr@gmail.com>
2022-07-02 15:25:09 +0300

    Логирование createClient

--- src/main/java/com/vilkov/PriceMonitoring/model/ClientHelper.java
+++ src/main/java/com/vilkov/PriceMonitoring/model/ClientHelper.java
@@ -3,29 +3,33 @@ package com.vilkov.PriceMonitoring.model;
 import com.vilkov.PriceMonitoring.model.dataStorage.DataStorage;
 import com.vilkov.PriceMonitoring.model.entity.Client;
 import com.vilkov.PriceMonitoring.model.entity.Message;
+import com.vilkov.PriceMonitoring.model.logger.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
-import java.util.logging.Logger;
+
 
 @Component
 public class ClientHelper {
 
     @Autowired
     DataStorage dataStorage;
-    Logger logger = Logger.getLogger("ClientHelper");
+    Logger logger = new Logger("Clients", "clients.txt");
 
     public Message createClient(Client client) {
         Client admin = dataStorage.getAdmin();
         if (clientAlreadyExists(client.getClientID())) {
-            return new Message(Status.ALREADY_EXIST, "Клиент уже существует");
+            logger.save(new Date() + ": Клиент " + client.getClientID() + " уже существует");
+            return new Message(Status.ALREADY_EXIST, " Клиент уже существует");
         }
 
         if (dataStorage.createEntity(client, admin)) {
-            return new Message(Status.SUCCESS, "Клиент успешно создан");
+            logger.save(new Date() + ": Клиент " + client.getClientID() + "создан");
+            return new Message(Status.SUCCESS, " Клиент успешно создан");
         } else {
-            logger.warning("Ошибка добавления клиента" + client.getClientID());
+            logger.save(new Date() + ": Ошибка добавления клиента" + client.getClientID());
             return new Message(Status.ERROR, "Ошибка добавления клиента");
         }
     }
